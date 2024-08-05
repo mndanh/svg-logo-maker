70eb1e18953d0d5d4ec4589727906cff7765be55
Dan Heer <mndanh@gmail.com>
2024-08-04 21:26:11 -0500

    added questions to prompt user

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+
+function UserQuestions() {
+    return [
+    {
+        type: 'input',
+        name: 'text',
+        message: "Enter 3 characters to add text to your logo.",
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: "What color do you want the text to be?"
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: "choose a shape",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: "What color do you want the shape to be?"
+    },
+    ]
+}
