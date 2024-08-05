2ba6dee43705381237eb0ae9a227f40c2049b0b2
Dan Heer <mndanh@gmail.com>
2024-08-05 09:46:45 -0500

    fixed issues with html colors. had them camel cased and needed them to be all lowercase

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Square, Triangle } = require("./lib/shapes.js");
 
 const htmlColor = [
-    "aliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet",
-    "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue",
-    "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid",
-    "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue",
-    "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod",
-    "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush",
-    "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink",
-    "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen",
-    "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue",
-    "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite",
-    "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed",
-    "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue",
-    "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow",
-    "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
-  ];
+    "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet",
+    "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue",
+    "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid",
+    "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue",
+    "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod",
+    "gray", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush",
+    "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink",
+    "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen",
+    "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue",
+    "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite",
+    "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred",
+    "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue",
+    "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow",
+    "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"
+];
 
 const isValidHexColor = (hex) => /^#([0-9A-F]{3}|[0-9A-F]{6})$/i.test(hex);
 const isHtmlColor = (color) => htmlColor.includes(color.toLowerCase());
 
 const validateColorInput = (color) => {
     if (isHtmlColor(color) || isValidHexColor(color)) {
-      return true;
+        return true;
     } else {
-      return "Invalid color input. Please enter a valid HTML color name or hex value.";
+        return "Invalid color input. Please enter a valid HTML color name or hex value.";
     }
-  };
+};
 
-  const validateTextInput = (input) => {
-    if(input.length > 3) {
-        return "Text input must be no more than 3 characters.";
-    }
-    return true;
-  };
+const validateTextInput = (input) => {
+if(input.length > 3) {
+    return "Text input must be no more than 3 characters.";
+}
+return true;
+};
   
-function UserQuestions() {
+function userQuestions() {
     return [
     {
         type: 'input',
@@ -53,7 +54,7 @@ function UserQuestions() {
     {
         type: 'list',
         name: 'shape',
-        message: "choose a shape",
+        message: "Choose a shape:",
         choices: ["Circle", "Triangle", "Square"]
     },
     {
@@ -66,3 +67,25 @@ function UserQuestions() {
 };
 
 // not going to create async await function until i've finished shapes.js
+async function init() {
+    const questions = userQuestions();
+    const replies = await inquirer.prompt(questions);
+    let shape;
+    if (replies.shape === "Circle") {
+        shape = new Circle(replies.text, replies.textcolor, replies.shapecolor);
+    } else if (replies.shape === "Square") {
+        shape = new Square(replies.text, replies.textcolor, replies.shapecolor);
+    } else if (replies.shape === "Triangle") {
+        shape = new Triangle(replies.text, replies.textcolor, replies.shapecolor);
+    }
+    const outputSVG = shape.printImg();
+    fs.writeFile('logo.svg', outputSVG, (err) => {
+        if (err) {
+            console.error("Could not generate SVG");
+        } else {
+            console.log("SVG generated");
+        }
+    });
+}
+
+init();
